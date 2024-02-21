74d2c82ef08707ecc421fb713b02f6332dc6f123
sara.hasan <sara.hasan@nextgeni.com>
2024-02-21 07:36:22 +0000

    gh-pages

--- src/containers/Home/Banner/index.tsx
+++ src/containers/Home/Banner/index.tsx
@@ -20,6 +20,33 @@ const settings = {
   centerMode: true,
   focusOnSelect: true,
   pauseOnHover: true,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 type HomeBannerProps = {};
